3379d0b5742a8126f31984f3e33c9a92ace84292
Viktor Syrotiuk <syrotiuk.pn@ucu.edu.ua>
2025-04-27 23:09:04 +0300

    fixes

--- b_tree.py
+++ b_tree.py
@@ -14,16 +14,6 @@ class BTreeNode:
         self.keys = []
         self.children = []
 
-    def display(self, level=0):
-        """
-        Displays the keys in the B-tree node.
-        :param level: The level of the node in the tree (used for indentation).
-        """
-        print(f"Level {level}: {self.keys}")
-        if not self.leaf:
-            for child in self.children:
-                child.display(level + 1)
-
 class BTree:
     """
     A B-tree data structure.
@@ -38,12 +28,6 @@ class BTree:
         """
         self.root = BTreeNode(True)
         self.t = t
-
-    def display(self):
-        """
-        Displays the B-tree structure starting from the root node.
-        """
-        self.root.display()
 
     def insert(self, k):
         """
@@ -108,18 +92,264 @@ class BTree:
 
         x.children.insert(i + 1, z)
 
-# def main():
-#     """
-#     Main function to demonstrate the B-tree implementation.
-#     """
-#     b = BTree(3)
+    def search(self, k, node=None):
+        """
+        Searches for a key in the B-tree.
+        :param k: The key to be searched.
+        :param node: The node to start the search from (defaults to root if None).
+        :return: A tuple (node, index) if found, otherwise None.
+        """
+        if node is None:
+            node = self.root
 
-#     keys = [10, 20, 5, 6, 12, 30, 7, 17]
-#     for key in keys:
-#         b.insert(key)
+        i = 0
+        while i < len(node.keys) and k > node.keys[i]:
+            i += 1
 
-#     print("B-tree structure:")
-#     b.display()
+        if i < len(node.keys) and k == node.keys[i]:
+            return (node, i)
 
-# if __name__ == '__main__':
-#     main()
+        if node.leaf:
+            return None
+
+        return self.search(k, node.children[i])
+
+    def delete(self, k):
+        """
+        Deletes a key from the B-tree.
+        :param k: The key to be deleted.
+        """
+        self._delete(self.root, k)
+
+        if self.root and len(self.root.keys) == 0 and not self.root.leaf:
+            self.root = self.root.children[0]
+
+    def _delete(self, node, k):
+        """
+        Recursive helper method to delete a key from a subtree.
+        :param node: The current node.
+        :param k: The key to be deleted.
+        """
+        t = self.t
+
+        i = 0
+        while i < len(node.keys) and k > node.keys[i]:
+            i += 1
+
+        if i < len(node.keys) and k == node.keys[i]:
+            if node.leaf:
+                node.keys.pop(i)
+                return
+
+            if len(node.children[i].keys) >= t:
+                pred = self._get_predecessor(node, i)
+                node.keys[i] = pred
+                self._delete(node.children[i], pred)
+
+            elif len(node.children[i + 1].keys) >= t:
+                succ = self._get_successor(node, i)
+                node.keys[i] = succ
+                self._delete(node.children[i + 1], succ)
+
+            else:
+                self._merge_children(node, i)
+                self._delete(node.children[i], k)
+
+        else:
+            if node.leaf:
+                return
+
+            child_index = i
+
+
+            if child_index > len(node.keys) - 1:
+                child_index = len(node.keys)
+
+            if len(node.children[child_index].keys) < t:
+                self._fill_child(node, child_index)
+
+                if child_index > 0 and child_index < len(node.children) and i == len(node.keys):
+                    self._delete(node.children[child_index - 1], k)
+                else:
+                    self._delete(node.children[child_index], k)
+            else:
+                self._delete(node.children[child_index], k)
+
+    def _get_predecessor(self, node, index):
+        """
+        Gets the predecessor of the key at the given index.
+        :param node: The node containing the key.
+        :param index: The index of the key.
+        :return: The predecessor key.
+        """
+        curr = node.children[index]
+        while not curr.leaf:
+            curr = curr.children[-1]
+        return curr.keys[-1]
+
+    def _get_successor(self, node, index):
+        """
+        Gets the successor of the key at the given index.
+        :param node: The node containing the key.
+        :param index: The index of the key.
+        :return: The successor key.
+        """
+        curr = node.children[index + 1]
+        while not curr.leaf:
+            curr = curr.children[0]
+        return curr.keys[0]
+
+    def _merge_children(self, node, index):
+        """
+        Merges the children at index and index+1.
+        :param node: The parent node.
+        :param index: The index of the left child.
+        """
+
+        left_child = node.children[index]
+        right_child = node.children[index + 1]
+
+        left_child.keys.append(node.keys[index])
+
+        left_child.keys.extend(right_child.keys)
+
+        if not left_child.leaf:
+            left_child.children.extend(right_child.children)
+
+        node.keys.pop(index)
+
+        node.children.pop(index + 1)
+
+    def _fill_child(self, node, index):
+        """
+        Fills the child at the given index with more keys.
+        :param node: The parent node.
+        :param index: The index of the child to fill.
+        """
+        t = self.t
+
+        if index > 0 and len(node.children[index - 1].keys) >= t:
+            self._borrow_from_prev(node, index)
+
+        elif index < len(node.children) - 1 and len(node.children[index + 1].keys) >= t:
+            self._borrow_from_next(node, index)
+
+        else:
+            if index == len(node.children) - 1:
+                self._merge_children(node, index - 1)
+            else:
+                self._merge_children(node, index)
+
+    def _borrow_from_prev(self, node, index):
+        """
+        Borrows a key from the previous child.
+        :param node: The parent node.
+        :param index: The index of the child that needs to borrow.
+        """
+        child = node.children[index]
+        sibling = node.children[index - 1]
+
+        child.keys.insert(0, node.keys[index - 1])
+
+        node.keys[index - 1] = sibling.keys.pop()
+
+        if not child.leaf:
+            child.children.insert(0, sibling.children.pop())
+
+    def _borrow_from_next(self, node, index):
+        """
+        Borrows a key from the next child.
+        :param node: The parent node.
+        :param index: The index of the child that needs to borrow.
+        """
+        child = node.children[index]
+        sibling = node.children[index + 1]
+
+
+        node.keys[index] = sibling.keys.pop(0)
+
+        if not sibling.leaf:
+            child.children.append(sibling.children.pop(0))
+
+    def inorder_traversal(self):
+        """
+        Performs in-order traversal of the B-tree.
+        :return: List of keys in in-order traversal.
+        """
+        result = []
+        self._inorder_traversal(self.root, result)
+        return result
+
+    def _inorder_traversal(self, node, result):
+        """
+        Helper method for in-order traversal.
+        :param node: The current node.
+        :param result: The list to store the traversal result.
+        """
+        i = 0
+        while i < len(node.keys):
+            if not node.leaf:
+                self._inorder_traversal(node.children[i], result)
+            result.append(node.keys[i])
+            i += 1
+
+        if not node.leaf and node.children:
+            self._inorder_traversal(node.children[-1], result)
+
+    def preorder_traversal(self):
+        """
+        Performs pre-order traversal of the B-tree.
+        :return: List of keys in pre-order traversal.
+        """
+        result = []
+        self._preorder_traversal(self.root, result)
+        return result
+
+    def _preorder_traversal(self, node, result):
+        """
+        Helper method for pre-order traversal.
+        :param node: The current node.
+        :param result: The list to store the traversal result.
+        """
+        if node:
+            result.extend(node.keys)
+
+            if not node.leaf:
+                for child in node.children:
+                    self._preorder_traversal(child, result)
+
+
+def main():
+    """
+    Main function to demonstrate the B-tree implementation.
+    """
+    b = BTree(3)
+
+    keys = [10, 20, 5, 6, 12, 30, 7, 17]
+    print("Вставляємо елементи:", keys)
+    for key in keys:
+        b.insert(key)
+
+
+    print("\nInorder обхід (відсортований порядок):", b.inorder_traversal())
+    print("Preorder обхід (корінь, потім діти):", b.preorder_traversal())
+
+    print("\nПошук елементів:")
+    for key in [6, 15, 30]:
+        result = b.search(key)
+        if result:
+            node, index = result
+            print(f"Елемент {key} знайдено в вузлі з ключами {node.keys} на позиції {index}")
+        else:
+            print(f"Елемент {key} не знайдено")
+
+    print("\nВидалення елементів:")
+    for key_to_delete in [6, 30]:
+        print(f"Видаляємо {key_to_delete}")
+        b.delete(key_to_delete)
+
+        print(f"Inorder обхід після видалення {key_to_delete}:", b.inorder_traversal())
+
+
+if __name__ == '__main__':
+    main()
