f0c555b9f4016ea37cad421f32c8411a0ad1115d
Viktor Syrotiuk <syrotiuk.pn@ucu.edu.ua>
2025-05-04 20:05:15 +0300

    tree sql implemented and docstrings added!

--- tree_factory.py
+++ tree_factory.py
@@ -1,25 +1,40 @@
+'''tree factory'''
 from tree_adapters import (
-    AVLTreeAdapter, 
-    RedBlackTreeAdapter, 
+    AVLTreeAdapter,
+    RedBlackTreeAdapter,
     SplayTreeAdapter,
     BTreeAdapter,
     TwoThreeTreeAdapter
 )
 
 class TreeFactory:
+    """
+    TreeFactory is a factory class that creates instances of different tree types.
+    It provides a static method to create a tree based on the specified type.
+    """
     @staticmethod
     def create_tree(tree_type):
+        """
+        Create a tree instance based on the specified tree type.
+        The tree type can be one of the following:
+        - "avl"
+        - "red-black"
+        - "splay"
+        - "b-tree"
+        - "2-3-tree"
+        The tree type is case-insensitive.
+        If an unknown tree type is specified, a ValueError is raised.
+        """
         tree_type = tree_type.lower()
-        
+
         if tree_type == "avl":
             return AVLTreeAdapter()
-        elif tree_type == "red-black":
+        if tree_type == "red-black":
             return RedBlackTreeAdapter()
-        elif tree_type == "splay":
+        if tree_type == "splay":
             return SplayTreeAdapter()
-        elif tree_type == "b-tree":
+        if tree_type == "b-tree":
             return BTreeAdapter(3)
-        elif tree_type == "2-3-tree":
+        if tree_type == "2-3-tree":
             return TwoThreeTreeAdapter()
-        else:
-            raise ValueError(f"Unknown tree type: {tree_type}")
+        raise ValueError(f"Unknown tree type: {tree_type}")
--- tree_sql.py
+++ tree_sql.py
@@ -1,96 +1,218 @@
+''''tree sql'''
 import argparse
 import shlex
 from data_manager import DataManager
 
 class TreeSQL:
+    """
+    TreeSQL -class for parsing and executing SQL-like commands.
+    This class provides an interface for creating databases, tables, and performing CRUD operations.
+    """
     def __init__(self):
-        """Ініціалізація класу TreeSQL"""
-        # Створити екземпляр менеджера даних
+        """
+        Initialize the TreeSQL class with a DataManager instance.
+        The DataManager is responsible for managing the databases and tables.
+        """
         self.data_manager = DataManager()
-    
-    def parse_command(self, command):
-        """Розбір та виконання SQL-подібної команди
-        
-        :param command: SQL-подібна команда для виконання
-        :return: Результат виконання команди
-        """
-        # TODO: Розділити команду на токени
-        # TODO: Привести всі токени до нижнього регістру, окрім рядків у лапках
-        # TODO: Розібрати команду за першим токеном (create, use, insert, select, update, delete)
-        pass
-    
-    def create_table_command(self, tokens):
-        """Розбір та виконання команди CREATE TABLE
-        
-        :param tokens: Список токенів команди, без "CREATE TABLE"
-        :return: Результат виконання команди
-        """
-        # TODO: Отримати назву таблиці
-        # TODO: Встановити тип дерева за замовчуванням (avl)
-        # TODO: Перевірити наявність USING та витягнути тип дерева
-        # TODO: Розібрати визначення стовпців
-        # TODO: Повернути результат виклику create_table з data_manager
-        pass
-    
-    def insert_command(self, tokens):
-        """Розбір та виконання команди INSERT
-        
-        :param tokens: Список токенів команди
-        :return: Результат виконання команди
-        """
-        # TODO: Перевірити правильність формату команди
-        # TODO: Отримати назву таблиці
-        # TODO: Розібрати значення
-        # TODO: Обробити значення (числа, рядки)
-        # TODO: Повернути результат виклику insert з data_manager
-        pass
-    
-    def select_command(self, tokens):
-        """Розбір та виконання команди SELECT
-        
-        :param tokens: Список токенів команди
-        :return: Відформатований результат запиту
-        """
-        # TODO: Отримати назву таблиці
-        # TODO: Розібрати умови, якщо є
-        # TODO: Викликати select з data_manager
-        # TODO: Відформатувати результат у вигляді таблиці
-        pass
-    
-    def update_command(self, tokens):
-        """Розбір та виконання команди UPDATE
-        
-        :param tokens: Список токенів команди
-        :return: Результат виконання команди
-        """
-        # TODO: Перевірити правильність формату команди
-        # TODO: Отримати назву таблиці
-        # TODO: Знайти WHERE, якщо є
-        # TODO: Розібрати частину SET (оновлення)
-        # TODO: Розібрати умову WHERE, якщо є
-        # TODO: Повернути результат виклику update з data_manager
-        pass
-    
-    def delete_command(self, tokens):
-        """Розбір та виконання команди DELETE
-        
-        :param tokens: Список токенів команди
-        :return: Результат виконання команди
-        """
-        # TODO: Перевірити правильність формату команди
-        # TODO: Отримати назву таблиці
-        # TODO: Розібрати умову WHERE, якщо є
-        # TODO: Повернути результат виклику delete з data_manager
-        pass
 
+    def parse_command(self, command):
+        """
+        Parse the input command and execute the corresponding action.
+        The command can be a SQL-like command for creating databases, tables, 
+        and performing CRUD operations.
+
+        :param command: str - The SQL-like command to be executed
+        :return: str - The result of the command execution or an error message.
+        """
+        tokens = shlex.split(command)
+        if not tokens:
+            return "Пуста команда"
+
+        cmd = tokens[0].lower()
+
+        if cmd == "create":
+            if len(tokens) >= 2 and tokens[1].lower() == "database":
+                return self.data_manager.create_database(tokens[2])
+            if len(tokens) >= 2 and tokens[1].lower() == "table":
+                return self.create_table_command(tokens[2:])
+        if cmd == "use":
+            return self.data_manager.use_database(tokens[1])
+        if cmd == "insert":
+            return self.insert_command(tokens)
+        if cmd == "select":
+            return self.select_command(tokens)
+        if cmd == "update":
+            return self.update_command(tokens)
+        if cmd == "delete":
+            return self.delete_command(tokens)
+        return "Невідома команда"
+
+    def create_table_command(self, tokens):
+        """
+        Create a table in the current database.
+        The command should be in the format: CREATE TABLE table_name (column1, column2, ...)
+        Optionally, a tree type can be specified using the "USING" keyword.
+        The default tree type is "avl".
+        
+        :param tokens: list - The tokens of the command after splitting
+        :return: str - The result of the table creation or an error message.
+        """
+        table_name = tokens[0]
+        tree_type = "avl"
+
+        if "using" in [t.lower() for t in tokens]:
+            idx = tokens.index("USING") if "USING" in tokens else tokens.index("using")
+            tree_type = tokens[idx + 1]
+
+        col_start = tokens.index("(")
+        col_end = tokens.index(")")
+        columns_str = ' '.join(tokens[col_start + 1: col_end])
+        columns = [col.strip() for col in columns_str.split(",")]
+
+        return self.data_manager.create_table(table_name, columns, tree_type)
+
+    def insert_command(self, tokens):
+        """
+        Insert a record into a table.
+        The command should be in the format: INSERT INTO table_name VALUES (value1, value2, ...)
+        The values should be separated by commas and can be strings, integers, or floats.
+
+        :param tokens: list - The tokens of the command after splitting
+        :return: str - The result of the insertion or an error message.
+        """
+        try:
+            table_name = tokens[2]
+            values_index = tokens.index("VALUES")
+            values_str = ' '.join(tokens[values_index + 1:])
+            values_str = values_str.strip("()")
+            raw_values = [v.strip() for v in values_str.split(",")]
+            parsed_values = [self._parse_value(v) for v in raw_values]
+            return self.data_manager.insert(table_name, parsed_values)
+
+        except Exception as e:
+            return f"Помилка вставки: {e}"
+
+    def select_command(self, tokens):
+        """
+        Select records from a table.
+        The command should be in the format: SELECT * FROM table_name WHERE condition
+        The condition is optional and can be used to filter the results.
+        
+        :param tokens: list - The tokens of the command after splitting
+        :return: str - The result of the selection or an error message.
+        """
+        try:
+            from_index = tokens.index("FROM")
+            table_name = tokens[from_index + 1]
+
+            condition = None
+            if "WHERE" in [t.upper() for t in tokens]:
+                where_index = tokens.index("WHERE")
+                condition = ' '.join(tokens[where_index + 1:])
+
+            result = self.data_manager.select(table_name, condition)
+            return "\n".join(str(r) for r in result)
+
+        except Exception as e:
+            return f"Помилка SELECT: {e}"
+
+    def update_command(self, tokens):
+        """
+        Update records in a table.
+        The command should be in the format: UPDATE table_name SET field1=value1, field2=value2
+        WHERE condition
+        The condition is optional and can be used to filter the records to be updated.
+
+        :param tokens: list - The tokens of the command after splitting
+        :return: str - The result of the update or an error message.
+        """
+        try:
+            table_name = tokens[1]
+            set_index = tokens.index("SET")
+            where_index = tokens.index("WHERE") if "WHERE" in tokens else len(tokens)
+
+            update_tokens = tokens[set_index + 1: where_index]
+            updates = {}
+            for item in ' '.join(update_tokens).split(','):
+                field, value = item.split('=')
+                updates[field.strip()] = value.strip().strip("'\"")
+
+            condition = ' '.join(tokens[where_index + 1:]) if "WHERE" in tokens else None
+
+            return self.data_manager.update(table_name, updates, condition)
+
+        except Exception as e:
+            return f"Помилка UPDATE: {e}"
+
+    def delete_command(self, tokens):
+        """
+        Delete records from a table.
+        The command should be in the format: DELETE FROM table_name WHERE condition
+        The condition is optional and can be used to filter the records to be deleted.
+
+        :param tokens: list - The tokens of the command after splitting
+        :return: str - The result of the deletion or an error message.
+        """
+        try:
+            table_name = tokens[2]
+            condition = None
+            if "WHERE" in [t.upper() for t in tokens]:
+                where_index = tokens.index("WHERE")
+                condition = ' '.join(tokens[where_index + 1:])
+
+            return self.data_manager.delete(table_name, condition)
+
+        except Exception as e:
+            return f"Помилка DELETE: {e}"
+
+    def _parse_value(self, value):
+        """
+        Helper method to parse a value from the command.
+        This method converts the value to its appropriate type (int, float, or string).
+        If the value is enclosed in single quotes, it is treated as a string.
+        If it can be converted to an integer or float, it is converted accordingly.
+        Otherwise, it is returned as a string.
+
+        :param value: str - The value to be parsed
+        :return: int, float, or str - The parsed value
+        """
+        if value.startswith("'") and value.endswith("'"):
+            return value.strip("'")
+        try:
+            return int(value)
+        except ValueError:
+            try:
+                return float(value)
+            except ValueError:
+                return value
 
 def main():
-    """Головна функція для обробки аргументів командного рядка"""
-    # TODO: Створити аргументи командного рядка
-    # TODO: Розібрати аргументи
-    # TODO: Створити екземпляр TreeSQL
-    # TODO: Виконати команду або запустити інтерактивний режим
-    pass
+    """
+    Main function to run the TreeSQL command line interface.
+    This function uses argparse to parse command line arguments and 
+    execute the corresponding actions.
+    """
+    parser = argparse.ArgumentParser(description="Інтерфейс команд для роботи з TreeSQL")
+    parser.add_argument("--cmd", type=str, help="SQL-подібна команда")
+    args = parser.parse_args()
+
+    sql = TreeSQL()
+
+    if args.cmd:
+        result = sql.parse_command(args.cmd)
+        print(result)
+    else:
+        print("Введіть команду або 'exit'):")
+        while True:
+            try:
+                command = input(">>> ")
+                if command.strip().lower() in ["exit", "quit"]:
+                    break
+                result = sql.parse_command(command)
+                print(result)
+            except KeyboardInterrupt:
+                print("\nВихід.")
+                break
 
 if __name__ == "__main__":
     main()
